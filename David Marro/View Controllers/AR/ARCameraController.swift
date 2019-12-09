031199b9a73d50c4c51b94792a2fc78f7dbe44e8
David Marro <davidmarro@MacBook-Pro-di-David.local>
2019-12-09 16:41:22 +0100

    sviluppo AR

--- David Marro/View Controllers/AR/ARCameraController.swift
+++ David Marro/View Controllers/AR/ARCameraController.swift
@@ -11,7 +11,7 @@ import ARKit
 import SceneKit
 
 
-class ARCameraController: UIViewController {
+class ARCameraController: UIViewController, ARSCNViewDelegate {
 
     
     @IBOutlet weak var sceneView: ARSCNView!
@@ -35,6 +35,13 @@ class ARCameraController: UIViewController {
     private func avviaSessione(){
 //        Avvio modalità AR
         let configuration = ARImageTrackingConfiguration()
+//        Creo manualmente la reference image da cercare nel mondo reale
+        let image = UIImage.init(named: "ImmagineDEmo")!
+        let referenceImage = ARReferenceImage.init(image.cgImage!, orientation: .up, physicalWidth: 0.05)
+//      La aggiungo alla configurazione AR
+        configuration.trackingImages = [referenceImage]
+        
+        sceneView.delegate  = self
         sceneView.session.run(configuration)
     }
     
@@ -47,6 +54,26 @@ class ARCameraController: UIViewController {
     
 // MARK: -FUNZIONI PRIVATE
     
+    
+// MARK: -ASRCNView delegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARImageAnchor else {
+            return
+        }
+        
+        print("Immagine trovata!")
+//        Torno sul thread principale
+    
+        DispatchQueue.main.async {
+            
+            
+            let scene = SCNScene.init(named: "book.scn")
+            let bookNode = scene?.rootNode.childNode(withName: "book", recursively: false)
+            node.addChildNode(bookNode!)
+            
+        }
+    }
 
 
 }
